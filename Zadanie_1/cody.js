2d764260c35e036513dd695f26f8176228c8e81e
Glasscorn <52011404+Glasscorn@users.noreply.github.com>
2019-10-10 19:16:26 +0300

    Add files via upload

--- Zadanie_1/cody.js
+++ Zadanie_1/cody.js
@@ -4,10 +4,35 @@ const hTButton = document.querySelector("#hashTags").addEventListener('click', h
 	  chessBoardButton = document.querySelector("#chessBoard").addEventListener('click', chessBoard);
 
 function hashTags(){
-	let b = "";
-	for (let a = 1; a <= 7; a++){
-		b += "#";
-		console.log(b);
+	let height = 15,
+		tags = "";
+	for (let a = 1; a <= height; a++){
+		function spaces(arg){
+				a === 1 || a === 3 ? a++ : a;
+				let space = (arg - (a-1))/2,
+					b = "";
+				if (space % 2 !== 0 && space % 1 !== 0){
+					space += 0.5;
+				} else if (space === 2){
+					space++;
+				}
+				if (space === 1){
+					space++;
+				}//Код решает проблему когда количество пробелов неверно
+				for (let i = 1; i <= space; i++) {
+					b += " ";
+				}
+				return b;
+			}
+		if (a === 1){
+			tags += "#";
+			string = spaces(height) + tags + spaces(height);
+			console.log(string);
+		} else if (a % 3 === 0) {
+			tags += "##";
+			string = spaces(height) + tags + spaces(height);
+			console.log(string);
+		}
 	}
 }
 
